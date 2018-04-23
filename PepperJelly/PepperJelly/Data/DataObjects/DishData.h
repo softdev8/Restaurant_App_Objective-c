3fab6fde7baff293ca912a7611bd711b82ff5fae
Mobile Developer <oncemoreforyou@outlook.com>
2018-04-23 20:55:36 +0800

    Update DishData.h

--- PepperJelly/PepperJelly/Data/DataObjects/DishData.h
+++ PepperJelly/PepperJelly/Data/DataObjects/DishData.h
@@ -3,7 +3,7 @@
 //  PepperJelly
 //
 //  Created by Evandro Harrison Hoffmann on 4/21/16.
-//  Copyright © 2016 DogTownMedia. All rights reserved.
+//  Copyright © 2016 ModileDev418. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
